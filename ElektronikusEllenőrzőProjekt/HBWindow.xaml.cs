a93cffe916247385f0fad22802d9d4786cc11637
Hajdú Kristóf <21.hajdu.kristof@mechwart.com>
2024-10-22 07:43:03 +0200

    90% done

--- ElektronikusEllenőrzőProjekt/HBWindow.xaml.cs
+++ ElektronikusEllenőrzőProjekt/HBWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System.IO;
 using System.Net.Http;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Animation;
 
 namespace ElektronikusEllenőrzőProjekt
 {
@@ -10,22 +12,164 @@ namespace ElektronikusEllenőrzőProjekt
     /// </summary>
     public partial class HBWindow : Window
     {
+        
         public HBWindow(string HFNev)
         {
             InitializeComponent();
             HNameDisplay.Content = HFNev;
-            TestAsync();
+            HFNEV = HFNev;
+            Beolvasas();
         }
 
-        List<int> Matek = new List<int>();
-        List<int> Irodalom = new List<int>();
-        List<int> Nyelvtan = new List<int>();
-        List<int> Töri = new List<int>();
-        List<int> Tesi = new List<int>();
-        List<int> Fizika = new List<int>();
-        List<int> Szakma = new List<int>();
-        List<int> IdegenNyelv = new List<int>();
+        public string HFNEV;
+        public float matekAtlag;
+        public float töriAtlag;
+        public float nyelvtanAtlag;
+        public float irodalomAtlag;
+        public float idegenAtlag;
+        public float szakmaAtlag;
+        public float tesiAtlag;
+        public float fizikaAtlag;
+        public float osszAtlag;
 
+        List<HConsturctor> adatok = new List<HConsturctor>();
+        private void Beolvasas()
+        {
+            foreach (var item in File.ReadAllLines("HAdatok.txt", Encoding.UTF8))
+            {
+                string[] resz = item.Split(';');
+
+                string nev = resz[0];
+                int om = Convert.ToInt32(resz[1]);
+                string matek = resz[2];
+                string töri = resz[3];
+                string nyelvtan = resz[4];
+                string irodalom = resz[5];
+                string idegen = resz[6];
+                string szakma = resz[7];
+                string tesi = resz[8];
+                string fizika = resz[9];
+
+                HConsturctor ujTanulo = new HConsturctor(nev, om, matek, töri, nyelvtan, irodalom, idegen, szakma, tesi, fizika);
+                adatok.Add(ujTanulo);
+            }
+        }
+        private void HAtlagok(int index)
+        {
+            string[] currentAtlag;
+            int HHossz;
+            int HHolder = 0;
+
+            currentAtlag = adatok[index].Matek.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz+3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            matekAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Töri.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                //MessageBox.Show(currentAtlag[i]);
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            töriAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Nyelvtan.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            nyelvtanAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Irodalom.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            irodalomAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Idegen.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            idegenAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Szakma.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            szakmaAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Tesi.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            tesiAtlag = HHolder / HHossz;
+
+            HHolder = 0;
+            currentAtlag = adatok[index].Fizika.Split(' ');
+            HHossz = currentAtlag.Length / 3;
+            for (int i = 0; i <= HHossz + 3; i += 3)
+                HHolder += Convert.ToInt32(currentAtlag[i]);
+            fizikaAtlag = HHolder / HHossz;
+        }
+        private void HFeltoltes()
+        {
+            HJegyekList.Items.Clear();
+            int index = 0;
+            for (int i = 0; i < adatok.Count; i++)
+            {
+                if (adatok[i].Nev == HFNEV)
+                    index = i;
+            }
+            HAtlagok(index);
+            string[] HSelectedJegyek = adatok[index].Matek.Split(',');
+            switch (HTargySelection.SelectedIndex)
+            {
+                case 0:
+                    HSelectedJegyek = adatok[index].Matek.Split(',');
+                    HAtlag.Content = $"Átlag : {matekAtlag}";
+                    break;
+                case 1:
+                    HSelectedJegyek = adatok[index].Töri.Split(',');
+                    HAtlag.Content = $"Átlag : {töriAtlag}";
+                    break;
+                case 2:
+                    HSelectedJegyek = adatok[index].Nyelvtan.Split(',');
+                    HAtlag.Content = $"Átlag : {nyelvtanAtlag}";
+                    break;
+                case 3:
+                    HSelectedJegyek = adatok[index].Irodalom.Split(',');
+                    HAtlag.Content = $"Átlag : {irodalomAtlag}";
+                    break;
+                case 4:
+                    HSelectedJegyek = adatok[index].Idegen.Split(',');
+                    HAtlag.Content = $"Átlag : {idegenAtlag}";
+                    break;
+                case 5:
+                    HSelectedJegyek = adatok[index].Szakma.Split(',');
+                    HAtlag.Content = $"Átlag : {szakmaAtlag}";
+                    break;
+                case 6:
+                    HSelectedJegyek = adatok[index].Tesi.Split(',');
+                    HAtlag.Content = $"Átlag : {tesiAtlag}";
+                    break;
+                case 7:
+                    HSelectedJegyek = adatok[index].Fizika.Split(',');
+                    HAtlag.Content = $"Átlag : {fizikaAtlag}";
+                    break;
+            }
+            Hooszatlag.Content = $"Össz átlag : {(matekAtlag + nyelvtanAtlag + irodalomAtlag + idegenAtlag + szakmaAtlag + tesiAtlag + fizikaAtlag) / 7}";
+            for (int i = 0; i < HSelectedJegyek.Length; i++)
+            {
+                ListBoxItem itm = new ListBoxItem();
+                itm.Content = HSelectedJegyek[i];
+                HJegyekList.Items.Add(itm);
+            }
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
@@ -41,46 +185,16 @@ namespace ElektronikusEllenőrzőProjekt
             else if (H5.IsChecked == true)
                 HSJegy = 5;
 
-            HListBox.Items.Add(new Label
-            {
-                Width = 240,
-                Height = 50,
-                Content = HSelectedTantargy.Text + " " + HSJegy + " " + HSelectedTipus.Text + " " + HHozzafuzes.Text,
-                HorizontalAlignment = HorizontalAlignment.Center
-            });
-            HAppendJegy(HSelectedTantargy.Text, HSJegy);
+            ListBoxItem itm = new ListBoxItem();
+            itm.Content = $"{HSJegy} {HSelectedTipus.Text} {HMegjegyzes.Text}";
+            HJegyekList.Items.Add(itm);
+
+
         }
 
-        private void HAppendJegy(string HTargy, int HJegy)
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            switch (HTargy)
-            {
-                case "Matematika":
-                    Matek.Append(HJegy); break;
-                case "Történelem":
-                    Töri.Append(HJegy); break;
-                case "Irodalom":
-                    Irodalom.Append(HJegy); break;
-                case "Nyelvtan":
-                    Nyelvtan.Append(HJegy); break;
-                case "Idegen nyelv":
-                    IdegenNyelv.Append(HJegy); break;
-                case "Szakma":
-                    Szakma.Append(HJegy); break;
-                case "Tesi":
-                    Tesi.Append(HJegy); break;
-                case "Fizika":
-                    Fizika.Append(HJegy); break;
-            }
-        }
-        private async Task TestAsync()
-        {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage(HttpMethod.Get, "https://023e-2001-4c4e-245b-4a00-3150-264-ad23-a94d.ngrok-free.app/GetTanulos");
-            var response = await client.SendAsync(request);
-            response.EnsureSuccessStatusCode();
-            Console.WriteLine(await response.Content.ReadAsStringAsync());
-            File.WriteAllText("asdasd.json", await response.Content.ReadAsStringAsync());
+            HFeltoltes();
         }
     }
 }
---
+++ ElektronikusEllenőrzőProjekt/HConsturctor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElektronikusEllenőrzőProjekt
+{
+    internal class HConsturctor
+    {
+        public HConsturctor(string nev, int oM, string matek, string töri, string nyelvtan, string irodalom, string idegen, string szakma, string tesi, string fizika)
+        {
+            Nev = nev;
+            OM = oM;
+            Matek = matek;
+            Töri = töri;
+            Nyelvtan = nyelvtan;
+            Irodalom = irodalom;
+            Idegen = idegen;
+            Szakma = szakma;
+            Tesi = tesi;
+            Fizika = fizika;
+        }
+
+        public string Nev {  get; set; }
+        public int OM { get; set; }
+        public string Matek { get; set; }
+        public string Töri { get; set; }
+        public string Nyelvtan { get; set; }
+        public string Irodalom { get; set; }
+        public string Idegen { get; set; }
+        public string Szakma { get; set; }
+        public string Tesi { get; set; }
+        public string Fizika { get; set; }
+
+
+
+    }
+}
